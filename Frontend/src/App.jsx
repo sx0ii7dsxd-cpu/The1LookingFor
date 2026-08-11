f2f65b8e23459f0391fe4038550d7b0f29e1a425
Siddharth <sx0ii7dsxd@gmail.com>
2026-08-11 13:57:28 +0530

    improvements

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,27 @@ import './App.css';
 function App() {
     return (
         <main className="container">
-            <h2>
-                <p className="label">The1LookingFor</p>
+            <h2 className="label">
+                The1LookingFor
             </h2>
-            <h1 className="heading">
-                We hire people for their thinking, not their history
-            </h1>
+            <h2 className="heading">
+                We hire people for their thinking, not their history 
+            </h2>
+            
+            <button className="button1">
+                Join "The1LookingFor" 
+            </button>
+            <section className="benifits">
+                <h2>Why Join Us?</h2>
+                <ul>
+                    <li>We treat everybody equal</li> 
+                    <li>No resumes required only skills</li>
+                    <li>Freshers have opportunities</li>
+                </ul>
+            </section>
+            <p className="description">
+                Companies will hire you based on your skills, not your resume. We provide a platform for freshers and experienced individuals to showcase their abilities and get hired by top companies.
+            </p>
         </main>
     );
 }
